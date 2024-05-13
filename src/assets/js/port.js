21c0566046822f183c6b0101eae6e465dfb950ee
vbnm134678 <syj1351@naver.com>
2024-05-13 20:43:10 +0900

    port영역 + img추가

--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -1,3 +1,21 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 export function port() {
-    console.log(port);
+    gsap.registerPlugin(ScrollTrigger);
+    const horSection = gsap.utils.toArray(".port__item");
+
+    gsap.to(horSection, {
+        xPercent: -120*(horSection.length-1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: "#port",
+            start: "top 56px",
+            end: "+=1000",
+            pin: true,
+            scrub: 1,
+            markers: false,
+            invalidateOnRefresh: true,
+            anticipatePin: 1,
+        }
+    });
 }
