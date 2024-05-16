4233076450e4e881dcc21cec4e98e70d29a6c6a1
vbnm134678 <syj1351@naver.com>
2024-05-16 18:42:00 +0900

    스크롤 smooth기능 추가 + link smooth기능 추가

--- src/assets/js/link.js
+++ src/assets/js/link.js
@@ -1,3 +1,17 @@
 export function link() {
+    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+        anchor.addEventListener("click", function(e){
+            e.preventDefault();
+
+            const targetId = this.getAttribute("href");
+            const targetElement = document.querySelector(targetId);
+
+            if(targetElement) {
+                targetElement.scrollIntoView({behavior:"smooth"});
+            }
+        })
+    })
     console.log(link)
 }
+// 바로 href태그로 이동하는 것을 막고 
+// 직접 href속성 값을 가져와 위치를 smooth를 통해 이동시키는 함수
--- src/assets/js/smooth.js
+++ src/assets/js/smooth.js
@@ -1,3 +1,20 @@
+import Lenis from "@studio-freight/lenis"
+
 export function smooth() {
-    console.log(smooth);
+    const lenis = new Lenis({
+        duration: 1,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10*t)),
+    });
+
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    lenis.on("scroll", (e) => {
+        console.log(e)
+    })
 }
+// Lenis를 이용한 smooth 효과
